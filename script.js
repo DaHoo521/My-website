50c9223b45a9b2956d4eb6158782783aedc0c73f
DaHoo521 <normanstewart521@gmail.com>
2025-01-15 13:05:23 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,41 @@
 async function fetchPosts() {
-    try {
-      const response = await fetch('http://artportfolio.infy.uk/wp-json/wp/v2/posts?_embed');
-      const posts = await response.json();
-  
-      let postsHtml = '';
-      posts.forEach((post, index) => {
-        if (index === 0) {
-          // Special layout for the first post (without image)
-          postsHtml += `
-            <div class="col-12 mb-5 text-center">
-              <h1>${post.title.rendered}</h1>
-              <div>${post.content.rendered}</div>
-            </div>
-          `;
-        } else {
-          // Default card layout for other posts
-          const imageUrl = post._embedded && post._embedded['wp:featuredmedia'] 
-                            ? post._embedded['wp:featuredmedia'][0].source_url 
-                            : 'default-image.jpg';
-  
-          postsHtml += `
-            <div class="col-md-4">
-              <div class="card mb-4">
-                <img src="${imageUrl}" class="card-img-top" alt="${post.title.rendered}">
-                <div class="card-body">
-                  <h5 class="card-title">${post.title.rendered}</h5>
-                  <p class="card-text">${post.excerpt.rendered}</p>
-                </div>
+  try {
+    const response = await fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://artportfolio.infy.uk/wp-json/wp/v2/posts?_embed'));
+    const data = await response.json();
+    const posts = JSON.parse(data.contents); // Parse the fetched content
+
+    let postsHtml = '';
+    posts.forEach((post, index) => {
+      if (index === 0) {
+        postsHtml += `
+          <div class="col-12 mb-5 text-center">
+            <h1>${post.title.rendered}</h1>
+            <div>${post.content.rendered}</div>
+          </div>
+        `;
+      } else {
+        const imageUrl = post._embedded && post._embedded['wp:featuredmedia']
+                          ? post._embedded['wp:featuredmedia'][0].source_url
+                          : 'default-image.jpg';
+
+        postsHtml += `
+          <div class="col-md-4">
+            <div class="card mb-4">
+              <img src="${imageUrl}" class="card-img-top" alt="${post.title.rendered}">
+              <div class="card-body">
+                <h5 class="card-title">${post.title.rendered}</h5>
+                <p class="card-text">${post.excerpt.rendered}</p>
               </div>
             </div>
-          `;
-        }
-      });
-  
-      document.getElementById('posts').innerHTML = postsHtml;
-    } catch (error) {
-      console.error('Error fetching posts:', error);
-    }
+          </div>
+        `;
+      }
+    });
+
+    document.getElementById('posts').innerHTML = postsHtml;
+  } catch (error) {
+    console.error('Error fetching posts:', error);
   }
-  
-  // Fetch posts on page load
-  fetchPosts();
-  
+}
+
+fetchPosts();
